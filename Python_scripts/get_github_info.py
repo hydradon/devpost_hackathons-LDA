9361e3c47e0b5b29a6c14d03eb4e13998e59888f
Quang <4397332+hydradon@users.noreply.github.com>
2020-02-07 16:22:42 -0700

    Add scripts to get github and gitlab data

---
+++ Python_scripts/get_github_info.py
@@ -0,0 +1,146 @@
+import requests
+import pandas as pd
+import time
+import os
+import json
+
+all_projects = pd.read_csv("../dataset/all_project_temp_Copy.csv")
+print("{} total softwares.".format(len(all_projects)))
+
+all_projects.dropna(subset=['hackathon_urls'], inplace=True)
+print("{} softwares submitted to a hackathon.".format(len(all_projects)))
+
+
+# drop apps do not have github/gitlab link
+all_projects.dropna(subset=['software_url'], inplace=True)
+all_projects = all_projects[all_projects.software_url.str.contains("github.com") | all_projects.software_url.str.contains("gitlab.com")]
+print("{} softwares that provide github/gitlab links.".format(len(all_projects)))
+
+# exit()
+# input("Test")
+# all_hackathons = pd.read_csv("./dataset/all_hackathons_temp.csv")
+# print("{} total hackathons.".format(len(all_hackathons)))
+# all_hackathons = all_hackathons[all_hackathons["is_ended"] == True]
+# print("{} hackathons have ended.".format(len(all_hackathons)))
+
+# GitHub
+GH_URL = "https://api.github.com"
+gh_head = {'Authorization': 'Bearer 44de7e8a01fff075e09e1714b47e57dbff17ed07',
+           'Accept'       : 'application/vnd.github.v3+json'}
+github_sleeptime = 2.5
+
+# GitLab
+GL_URL = "https://gitlab.com/api/v4/projects"
+gl_head = {'Authorization': 'Bearer ee1PrQS7GNJaokkruo5n'}
+gitlab_sleeptime = 0.5
+
+
+json_res = requests.get(url = GH_URL + "/rate_limit", headers=gh_head).json()
+print("Current limit: " + str(json_res['resources']['core']['remaining']))
+
+num_inaccessible_github_repos = 0
+num_inaccessible_gitlab_repos = 0
+
+for i, row in all_projects.iterrows():
+    if pd.isna(row['software_url']):
+        continue
+    software_urls = row['software_url'].lower().split("||")
+
+    for url in software_urls:
+        if "github.com" in url: #NOTE GitHub rate limit is 5000 per hour (or 83 per minute)
+            print("Found github link {} for project {}.".format(url, row['project_name']))
+            url_comp = url.split("/")
+            user = url_comp[3]
+            repo = url_comp[4]
+            print("Username: " + user + ". Github repo: " + repo)
+            
+            # Overall information: GH_URL + "/repos/{user}/{repo_name}"
+            for what in ['commits', 
+                         'pulls', 
+                         'pulls_comments',
+                         'stargazers',
+                         'forks',
+                         'events',
+                         'contributors',
+                         'issues',
+                         'issues_events',
+                         'issues_comments']:
+            
+                # query github api for commits information:  GH_URL + "/repos/{user}/{repo_name}/commits"
+                print("==Querying for {}.".format(what))
+                target_url = GH_URL + "/repos/{}/{}/{}".format(user, repo, what.replace("_", "/"))
+                print("==Target url: {}".format(target_url))
+
+                response = requests.get(url = target_url, headers=gh_head)
+
+                if response:
+                    json_res = response.json()
+
+                    json_file_name = "../githubdata/{}/{}_{}_{}_{}.json".format(what, user, repo, "github", what)
+                    os.makedirs(os.path.dirname(json_file_name), exist_ok=True)
+                    with open(json_file_name, 'w+') as outfile:
+                        json.dump(json_res, outfile, indent=4)
+                else:
+                    num_inaccessible_github_repos += 1
+                    print("Inaccessible github repo!! Error code: {}.".format(response.status_code))
+            
+                time.sleep(github_sleeptime)
+
+        elif "gitlab.com" in url: #NOTE GitLab rate limit is 600 per minute
+            # Sample gitlab link: https://gitlab.com/gitlab-org/gitlab
+            # Issues:  https://gitlab.com/api/v4/projects/gitlab-org%2Fgitlab/issues?scope=all
+            # Commits: https://gitlab.com/api/v4/projects/gitlab-org%2Fgitlab/repository/commits
+            # Merge requests: https://gitlab.com/api/v4/projects/gitlab-org%2Fgitlab/merge_requests?scope=all
+            # Forks: https://gitlab.com/api/v4/projects/gitlab-org%2Fgitlab/forks
+            # Starrers: https://gitlab.com/api/v4/projects/gitlab-org%2Fgitlab/starrers
+            # Events: https://gitlab.com/api/v4/projects/gitlab-org%2Fgitlab/events
+            # Contributors: https://gitlab.com/api/v4/projects/gitlab-org%2Fgitlab/repository/contributors
+            print("Found gitlab link {} for project {}.".format(url, row['project_name']))
+            url_comp = url.split("/")
+            user = url_comp[3]
+            repo = url_comp[4]
+            print("Username: " + user + ". Github repo: " + repo)
+
+            api_param = {
+                "issues"       : "issues?scope=all",
+                "commits"      : "repository/commits",
+                "pulls"        : "merge_requests?scope=all",
+                "forks"        : "forks",
+                "stargazers"   : "starrers",
+                "events"       : "events",
+                "contributors" : "repository/contributors"
+            }
+
+            for what in api_param:
+                print("==Querying for {}.".format(what))
+                target_url = GL_URL + "/{}%2F{}/{}".format(user, repo, api_param[what])
+                print("==Target url: {}".format(target_url))
+
+                response = requests.get(url = target_url, headers=gl_head)
+                # input("Here...")
+                if response:
+                    json_res = response.json()
+
+                    json_file_name = "../gitlabdata/{}/{}_{}_{}_{}.json".format(what, user, repo, "gitlab", what)
+                    os.makedirs(os.path.dirname(json_file_name), exist_ok=True)
+                    with open(json_file_name, 'w+') as outfile:
+                        json.dump(json_res, outfile, indent=4)
+                else:
+                    num_inaccessible_gitlab_repos += 1
+                    print("Inaccessible gitlab repo!! Error code: {}.".format(response.status_code))
+            
+                time.sleep(gitlab_sleeptime)
+
+        json_res = requests.get(url = GH_URL + "/rate_limit", headers=gh_head).json()
+        print("Current limit: " + str(json_res['resources']['core']['remaining']))   
+        # input("Here......")     
+
+# print("Number of inaccessible Github repos: {}.".format(num_inaccessible_github_repos))
+# print("Number of inaccessible Gitlab repos: {}.".format(num_inaccessible_gitlab_repos))
+
+# Write to file
+# output = "./dataset/all_project_temp_with_github_info.csv"
+# if os.path.exists(output):
+#     os.remove(output)
+
+# all_projects.to_csv(output, encoding='utf-8-sig', index=False)
---
+++ Python_scripts/selecting_apps_for_model.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os
+import numpy as np
+
+# df_apps = pd.read("../dataset/all_project_cleaned.csv")
+df_apps = pd.read_csv("../dataset/all_project_temp.csv")
+df_hackathons = pd.read_csv("../dataset/all_hackathons.csv")
+
+# only get apps that are submitted to a hackathon
+df_apps.dropna(subset=['hackathon_urls'], inplace=True)
+
+# drop hackathons that have not ended at the moment
+df_hackathons = df_hackathons[df_hackathons.is_ended == True]
+
+# retain games that are submitted to hackathon that have ended
+ended_hackathon_urls = df_hackathons.url.tolist()
+print(len(ended_hackathon_urls))
+temp = df_apps['hackathon_urls'].str.split('\|+', expand=True).stack().isin(ended_hackathon_urls).groupby(level=0).all()
+df_apps = df_apps[temp]
+
+# Get later hackathon end date
+
+
+
+
+# Calculate number of developer
+df_apps["num_devs"] = df_apps["author"].map(lambda a: len(a.split("||")) if pd.notna(a) else 0)
+
+# Calculate number of hackathons
+df_apps["num_hackathons"] = df_apps["hackathon_names"].map(lambda a: len(a.split("||")) if pd.notna(a) else 0)
+
+# Calculate number of winning titles
+df_apps["num_titles"] = df_apps["win_titles"].map(lambda a: len([i for i in a.replace("<>", "||").split("||") if i]) if pd.notna(a) else 0)
+
+# Calculate number of technology used
+df_apps["num_buildWiths"] = df_apps["build_with"].map(lambda a: len(a.split("||")) if pd.notna(a) else 0)
+
+
+# Write to file
+output = "../dataset/all_project_ended_hack.csv"
+if os.path.exists(output):
+    os.remove(output)
+
+df_apps.to_csv(output, encoding='utf-8-sig', index=False)
